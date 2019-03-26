5a20720f87bd514efd17a40b68409e658067e472
Paul Traylor <kungfudiscomonkey@gmail.com>
2019-03-26 17:05:10 +0900

    Add default user-agent

--- grafana/client.py
+++ grafana/client.py
@@ -3,6 +3,8 @@ import os
 import requests
 from requests.auth import AuthBase
 
+from django.conf import settings
+
 
 class GrafanaAuth(AuthBase):
     def __init__(self, org):
@@ -10,6 +12,7 @@ class GrafanaAuth(AuthBase):
 
     def __call__(self, r):
         r.headers["Authorization"] = "Bearer " + os.environ["GRAFANA_TOKEN"]
+        r.headers["User-agent"] = getattr(settings, "USER_AGENT", "django-grafana")
         return r
 
 
